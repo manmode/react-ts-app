fb316ccd3ef664024de6a32f38abf94b86fc15d4
Hiralal Manmode <hiralal.manmode@oracle.com>
2023-06-03 23:17:28 -0400

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import List from './components/DemoPage/List/List';
 import View from './components/DemoPage/View/View';
 function App() {
   return (
-    <div className="flex">
-      <List className="w-1/5 h-screen p-4"/>
-      <View className="w-4/5 h-screen border-l-4 border-blue-400 p-4"/>
-    </div>
+    <>
+      <div className="flex w-full py-8 place-content-center bg-gradient-to-r from-cyan-500 to-blue-500">
+        <h3 className="text-white font-bold text-2xl">HM Components</h3>
+      </div>
+      <div className="flex">
+        <List className="w-1/5 h-screen p-4" />
+        <View className="w-4/5 h-screen border-l-4 border-blue-400 p-4" />
+      </div>
+    </>
   );
 }
 
